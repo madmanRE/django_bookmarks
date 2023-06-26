e87ed597f8133695d458f96eac97505d94db2d83
Roman <121165911+madmanRE@users.noreply.github.com>
2023-06-26 12:35:08 +0300

    реализовал регистрацию

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -10,6 +11,7 @@ class LoginForm(forms.Form):
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Повторите пароль", widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ["username", "first_name", "email"]
@@ -20,11 +22,24 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Пароли не совпадают')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Данный адерс электронной почты уже используется')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError('Адрес электронной почты уже используется')
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 
@@ -57,6 +58,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Ваш профиль обновлен', 'успешно')
+        else:
+            messages.error(request, 'Возникла ошибка при обновлении профиля')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)

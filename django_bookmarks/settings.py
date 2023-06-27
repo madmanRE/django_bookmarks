d1fe381a40f3f6bf3a13ea637607ca1d10840ec9
Roman <121165911+madmanRE@users.noreply.github.com>
2023-06-27 22:53:59 +0300

    Создание букмарклета

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "social_django",
     "django_extensions",
+    "images.apps.ImagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,9 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type('application/javascript', '.js', True)
+    mimetypes.add_type('text/css', '.css', True)
